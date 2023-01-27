a6997fda9e05bc5dbb8aadc2d97f4ee53543e3a6
unknown <promotion.kang.jay@gmail.com>
2023-01-27 16:15:43 -0600

    update server url

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server -> bot's reponse
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codex-vnez.onrender.com', {
     method: 'POST',
     headers: {
       'Content-type': 'application/json'
